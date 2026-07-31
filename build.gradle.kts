8b9f7afff35342abbb0ea0a8aee6c43eb91a0008
Myron Shykhov <myronshykhov@gmail.com>
2026-07-31 06:22:03 +0300

    feat(observability): export traces over OTLP

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
 
     // Observability
     implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
 
     // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
